55fd0deea00f4f7138af104698cbd6c2b769fb24
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-06-01 21:05:22 +0900

    [Fix] #9 Fix Code Error

--- project/src/App.js
+++ project/src/App.js
@@ -2,24 +2,22 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
   {path: '/', element: <Homepage />, errorElement: <ErrorPage />, children: [
-    {path: 'search', element: <SearchPage />, children: [
-      {path: 'result', element: <SearchResultPage />}
-    ]},
+    {path: 'search', element: <SearchPage />},
+    {path: 'search/result', element: <SearchResultPage />},
     {path: 'challenge', element: <ChallengePage /> },
-    {path: 'auth', element: <AuthenticationPage />, children: [
-      {path: 'findpw', element: <FindPwPage />}
-    ]},
+    {path: 'auth', element: <AuthenticationPage /> },
+    {path: 'auth/findpw', element: <FindPwPage /> },
     {path: 'mypage', element: <MyPage />, children: [
       {path: 'account', element: <AccountPage />},
       {path: 'recent', element: <RecentReadsPage />},
       {path: 'tobe', element: <ToBeReadsPage />},
-      {path: 'mychallenge', element: <MyChallengePage />, children: [
+      {path: 'mychallenge', element: <ChallengeRoot />, children: [
+        {index: true, element: <MyChallengePage />},
         {path: 'new', element: <NewChallengePage />},
         {path: ':challengeId', element: <MyChallengeDetailPage />}
       ]},
-      {path: 'review', element: <ReviewPage />, children: [
-        {path: 'new', element: <NewReviewPage />}
-      ]},
+      {path: 'review', element: <ReviewPage />},
+      {path: 'review/new', element: <NewReviewPage />},
       {path: 'memo', element: <MemoPage />}
     ]}
   ]}
