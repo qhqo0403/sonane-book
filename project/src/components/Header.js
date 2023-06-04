75d319dd1889c3d0db2fcc968639bb5797fc983b
Yejin-Han <yain3333@gmail.com>
2023-06-05 03:42:51 +0900

    Squashed commit of the following:
    commit 86908d960fa04d90ac45b728a170b2194618916f
    Author: Yejin-Han <yain3333@gmail.com>
    Date:   Mon Jun 5 03:37:01 2023 +0900
    
        [Feat] #18 Tailwind CSS settings
    
        tailwind css 사용을 위한 설정
    
        추가 수정 사항
        header, footer의 기본 스타일링 css 파일-> tailwind식 클래스 지정으로 변경
    
    commit fc6133d4beaef70482d929eb383c3e8c573d910a
    Author: Yejin-Han <yain3333@gmail.com>
    Date:   Sun Jun 4 23:58:51 2023 +0900
    
        [Feat] #12 Add Footer
    
        Footer.js와 Root.css에 footer 관련 코드 추가
    
        추가 수정 사항
        1. MainNavigation -> Header로 파일명 수정 및 해당 코드 수정
    
    commit 46458f3a9b063e9f6152fb9a2af953ffe0c00f8b
    Merge: e85f6c8 3ab18ea
    Author: qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
    Date:   Sun Jun 4 23:26:32 2023 +0900
    
        Merge pull request #17 from qhqo0403/_Feat_Function/issue-11
    
        [Feat] #11 Add Header
    
    commit 3ab18ea6bd658c3f78183cd87b27095cadcaea9d
    Author: Yejin-Han <yain3333@gmail.com>
    Date:   Sun Jun 4 22:41:37 2023 +0900
    
        [Feat] #11 Add Header
    
        header를 크게 tnb와 gnb로 나누어 tnb에 로그인, 회원가입/마이페이지 gnb에 로고와 메인내비게이션을 담았음.
    
        추가 수정 사항
        1. RootLayout을 만들고 공통 컴포넌트인 header와 Outlet으로 동작하게 함, HomePage는 children index:true로 내려줌
        2. 각 페이지들이 빈 페이지라 아무것도 확인할 수 없는 오류가 떠서 긴급하게 rafce 작성해줌.
    
        주의사항
        1. 일단 기본 css로 스타일링 하였으나 추후 tailwind로 수정할 예정
    
    commit e85f6c8c1f5494ea0133e3039da4ef50ab33ce64
    Merge: 13677e0 987bceb
    Author: Yejin Han (한예진) <119913434+Yejin-Han@users.noreply.github.com>
    Date:   Sun Jun 4 14:40:22 2023 +0900
    
        Merge pull request #15 from qhqo0403/_Feat_Function/issue-14
    
        [Feat] #14 Create My Page and Account Page
    
    commit 987bceb5ceb05f38aa75c8d4e1673c095d1a7fba
    Author: qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
    Date:   Sun Jun 4 02:59:21 2023 +0900
    
        [Feat] #14 Create My Page and Account Page
    
        1) tailwind를 적용하기전 임시적으로 기본 css 를 사용하였음 (추후 수정 예정)
        2) 라우트 객체 정의에 사용된 엘리먼트 페이지가 존재하지 않았기 때문에 발생하는 오류를 없애기 위해 내용이 없는 기본 파일만 생성하였음 ( export가 안된 상황이기 때문에 여전히 오류는 존재하지만 만든 페이지의 기능을 확인하는데에는 문제가 없음)

--- project/src/components/Header.js
+++ project/src/components/Header.js
@@ -1,18 +1,16 @@
 import { useState } from "react";
 import { NavLink, Link } from "react-router-dom";
 
-import "./Root.css";
-
 const Header = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
-    <header>
-      <div className="tnb">
-        <ul>
+    <header className="w-screen fixed top-0 left-0 pb-5 px-8">
+      <div className="my-2">
+        <ul className="flex justify-end">
           {!isLoggedIn ? (
             <>
-              <li>
+              <li className="mr-3">
                 <NavLink to="auth">로그인</NavLink>
               </li>
               <li>
@@ -26,18 +24,18 @@ const Header = () => {
           )}
         </ul>
       </div>
-      <div className="gnb">
+      <div className="flex justify-between items-center py-4">
         <h1>
           <Link to="">로고</Link>
         </h1>
         <nav>
-          <ul>
-            <li>
+          <ul className="flex">
+            <li className="mr-8">
               <NavLink to="" end>
                 Home
               </NavLink>
             </li>
-            <li>
+            <li className="mr-8">
               <NavLink to="search">Search</NavLink>
             </li>
             <li>
