86908d960fa04d90ac45b728a170b2194618916f
Yejin-Han <yain3333@gmail.com>
2023-06-05 03:37:01 +0900

    [Feat] #18 Tailwind CSS settings
    tailwind css 사용을 위한 설정
    
    추가 수정 사항
    header, footer의 기본 스타일링 css 파일-> tailwind식 클래스 지정으로 변경

--- project/src/components/Footer.js
+++ project/src/components/Footer.js
@@ -1,8 +1,6 @@
-import "./Root.css";
-
 const Footer = () => {
   return (
-    <footer>
+    <footer className="absolute left-1/2 bottom-2 -translate-x-1/2">
       <p>&copy; 2023. 손 안의 도서관 all rights reserved.</p>
     </footer>
   );
